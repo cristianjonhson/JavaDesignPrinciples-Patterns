d8b69e517f5104a43f9046579a2e2a2e7e5d259f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-09-02 23:28:54 -0400

    arquitectura base proyecto

---
+++ src/main/java/cl/edu/galaxy/training/JavaDesignPrinciplesPatternsApplication.java
@@ -0,0 +1,13 @@
+package cl.edu.galaxy.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignPrinciplesPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesignPrinciplesPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/edu/galaxy/training/JavaDesignPrinciplesPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.edu.galaxy.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesignPrinciplesPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
