541e3d69c4870523678973b2521279574bed2f86
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-09-06 23:23:22 -0400

    Crear Constantes tipo Controllers

---
+++ src/main/java/cl/edu/galaxy/training/controllers/commons/Constants.java
@@ -0,0 +1,18 @@
+package cl.edu.galaxy.training.controllers.commons;
+
+public final class Constants {
+
+    private Constants() {
+        // Evita la instanciación
+    }
+
+    public static final String API_ESPECIES = "/api/especies";
+    public static final String API_RAZAS = "/api/razas";
+    public static final String API_VACUNAS = "/api/vacunas";
+    public static final String API_MASCOTAS = "/api/mascotas";
+    public static final String API_VETERINARIOS = "/api/veterinarios";
+    public static final String API_CITAS = "/api/citas";
+    public static final String API_HISTORIAL_VACUNAS = "/api/historial-vacunas";
+
+    public static final String PATH_ID = "/{id}";
+}
